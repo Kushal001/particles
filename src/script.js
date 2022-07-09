ef5d33b8aed90803720dc8bde56e762da8e0473c
Kushal001 <kushal121320@gmail.com>
2022-07-09 19:47:52 +0545

    something

--- src/script.js
+++ src/script.js
@@ -1,145 +1,121 @@
-import './style.css'
-import * as THREE from 'three'
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
+import "./style.css"
+import * as THREE from "three"
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js"
+import * as dat from "dat.gui"
+import { Raycaster } from "three"
 
 /**
  * Base
  */
 // Debug
 const gui = new dat.GUI()
+gui.closed = true
 
 // Canvas
-const canvas = document.querySelector('canvas.webgl')
+const canvas = document.querySelector("canvas.webgl")
 
 // Scene
 const scene = new THREE.Scene()
 
-/**
- * Galaxy
- */
-const parameters = {}
-parameters.count = 200000
-parameters.size = 0.005
-parameters.radius = 5
-parameters.branches = 3
-parameters.spin = 1
-parameters.randomness = 0.5
-parameters.randomnessPower = 3
-parameters.insideColor = '#ff6030'
-parameters.outsideColor = '#1b3984'
+// Raycaster
+const raycaster = new Raycaster()
 
-let geometry = null
-let material = null
-let points = null
-
-const generateGalaxy = () =>
-{
-    if(points !== null)
-    {
-        geometry.dispose()
-        material.dispose()
-        scene.remove(points)
-    }
-
-    /**
-     * Geometry
-     */
-    geometry = new THREE.BufferGeometry()
-
-    const positions = new Float32Array(parameters.count * 3)
-    const colors = new Float32Array(parameters.count * 3)
-
-    const insideColor = new THREE.Color(parameters.insideColor)
-    const outsideColor = new THREE.Color(parameters.outsideColor)
-
-    for(let i = 0; i < parameters.count; i++)
-    {
-        const i3 = i * 3
-
-        // Position
-        const radius = Math.random() * parameters.radius
-
-        const branchAngle = (i % parameters.branches) / parameters.branches * Math.PI * 2
-
-        const randomX = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
-        const randomY = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
-        const randomZ = Math.pow(Math.random(), parameters.randomnessPower) * (Math.random() < 0.5 ? 1 : - 1) * parameters.randomness * radius
-
-        positions[i3    ] = Math.cos(branchAngle) * radius + randomX
-        positions[i3 + 1] = randomY
-        positions[i3 + 2] = Math.sin(branchAngle) * radius + randomZ
-
-        // Color
-        const mixedColor = insideColor.clone()
-        mixedColor.lerp(outsideColor, radius / parameters.radius)
-
-        colors[i3    ] = mixedColor.r
-        colors[i3 + 1] = mixedColor.g
-        colors[i3 + 2] = mixedColor.b
-    }
-
-    geometry.setAttribute('position', new THREE.BufferAttribute(positions, 3))
-    geometry.setAttribute('color', new THREE.BufferAttribute(colors, 3))
-
-    /**
-     * Material
-     */
-    material = new THREE.PointsMaterial({
-        size: parameters.size,
-        sizeAttenuation: true,
-        depthWrite: false,
-        blending: THREE.AdditiveBlending,
-        vertexColors: true
-    })
-
-    /**
-     * Points
-     */
-    points = new THREE.Points(geometry, material)
-    scene.add(points)
-}
-
-generateGalaxy()
-
-gui.add(parameters, 'count').min(100).max(1000000).step(100).onFinishChange(generateGalaxy)
-gui.add(parameters, 'radius').min(0.01).max(20).step(0.01).onFinishChange(generateGalaxy)
-gui.add(parameters, 'branches').min(2).max(20).step(1).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomness').min(0).max(2).step(0.001).onFinishChange(generateGalaxy)
-gui.add(parameters, 'randomnessPower').min(1).max(10).step(0.001).onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'insideColor').onFinishChange(generateGalaxy)
-gui.addColor(parameters, 'outsideColor').onFinishChange(generateGalaxy)
+// Mouse
+const mouse = new THREE.Vector2()
+const point = new THREE.Vector2()
 
 /**
  * Sizes
  */
 const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
+  width: window.innerWidth,
+  height: window.innerHeight,
 }
 
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+/**
+ * Bg
+ */
+const bg = new THREE.Mesh(
+  new THREE.PlaneBufferGeometry(25, 25, 128, 128),
+  new THREE.MeshBasicMaterial({ color: "#000" })
+)
+scene.add(bg)
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+/**
+ * Particle
+ */
+let particleGeometry, particleMaterial, dot
+const createParticle = () => {
+  //   if (dot !== null) {
+  //     scene.remove(dot)
+  //   }
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  particleGeometry = new THREE.PlaneBufferGeometry(3, 3, 32, 32)
+
+  particleMaterial = new THREE.ShaderMaterial({
+    uniforms: {
+      uTime: { value: 0 },
+    },
+
+    vertexShader: `
+            uniform float uTime;
+    
+            void main() {
+                vec4 modelPosition = modelMatrix * vec4(position, 1.);
+    
+                modelPosition.x = sin(modelPosition.x * 2. * 3.14);
+                modelPosition.y = cos(modelPosition.x * 2. * 3.14);
+    
+                vec4 viewPosition = viewMatrix * modelPosition;
+                vec4 projectionPosition = projectionMatrix * viewPosition;
+    
+                gl_Position = projectionPosition;
+    
+                // Points
+                gl_PointSize = 1.;
+            }
+        `,
+    fragmentShader: `
+            void main() {
+                gl_FragColor = vec4(vec3(1.), 1.);
+            }
+        `,
+  })
+
+  dot = new THREE.Points(particleGeometry, particleMaterial)
+  scene.add(dot)
+
+  dot.position.set(point.x, point.y)
+}
+
+window.addEventListener("resize", () => {
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
+
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
+
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 /**
  * Camera
  */
-// Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 3
-camera.position.y = 3
+const aspectRatio = sizes.width / sizes.height
+const camera = new THREE.OrthographicCamera(
+  -4 * aspectRatio,
+  4 * aspectRatio,
+  4,
+  -4,
+  0.1,
+  100
+)
+// const camera = new THREE.PerspectiveCamera(75, aspectRatio, 1, 100)
+
 camera.position.z = 3
 scene.add(camera)
 
@@ -148,10 +124,20 @@ const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
 /**
+ * Mouse move
+ */
+window.addEventListener("mousemove", (event) => {
+  mouse.x = (event.clientX / sizes.width) * 2 - 1
+  mouse.y = -(event.clientY / sizes.height) * 2 + 1
+
+  createParticle()
+})
+
+/**
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+  canvas: canvas,
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
@@ -161,18 +147,33 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
  */
 const clock = new THREE.Clock()
 
-const tick = () =>
-{
-    const elapsedTime = clock.getElapsedTime()
+const tick = () => {
+  const elapsedTime = clock.getElapsedTime()
 
-    // Update controls
-    controls.update()
+  // Cast ray
+  raycaster.setFromCamera(mouse, camera)
 
-    // Render
-    renderer.render(scene, camera)
+  const objectsToTest = [bg]
+  const intersects = raycaster.intersectObjects(objectsToTest)
 
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
+  if (intersects.length > 0) {
+    point.x = intersects[0].point.x
+    point.y = intersects[0].point.y
+  }
+
+  // Update controls
+  controls.update()
+
+  //   Update Materials
+  if (particleMaterial) {
+    particleMaterial.uniforms.uTime.value = elapsedTime
+  }
+
+  // Render
+  renderer.render(scene, camera)
+
+  // Call tick again on the next frame
+  window.requestAnimationFrame(tick)
 }
 
 tick()
